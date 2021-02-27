7b7b3bb208eb1947c79d93d1e5dd60a14523161d
asrilpermana <asrillper@gmail.com>
2021-02-27 10:45:48 +0700

    menyelesaikan tugas1 praktikum

---
+++ praktikum/tugas1/latihan1a.php
@@ -0,0 +1,15 @@
+<!-- 
+Nama : Asril Dwi Cahya Permana
+NRP : 203040118
+Shift Praktikum : Jum'at pukul 13:00
+ -->
+
+<?php
+
+for( $i = 1; $i <= 3; $i++ ) {
+    for( $j = 1; $j <= 3; $j++ ) {
+        echo "Ini perulangan ke ($i,$j) <br>";
+    }
+}
+
+?>
