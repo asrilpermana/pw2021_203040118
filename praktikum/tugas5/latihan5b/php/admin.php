7716f60e241d362aa8ad8f6a7b6a1de91377be87
asrilpermana <asrillper@gmail.com>
2021-04-15 16:11:07 +0700

    menyelesaikan tugas5 praktikum

---
+++ praktikum/tugas5/latihan5b/php/admin.php
@@ -0,0 +1,61 @@
+<!-- 
+Nama : Asril Dwi Cahya Permana
+NRP : 203040118
+Shift Praktikum : Jum'at pukul 13:00
+ -->
+
+<?php
+    require 'functions.php';
+    $barang = query("SELECT * FROM barang")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 5b</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+
+    <div class="add">
+        <a href="tambah.php"><button>Tambah Data</button></a>
+    </div>
+    
+    <br>
+
+    <table border="1" cellpadding="13" cellspacing="0">
+        <tr>
+            <th>No</th>
+            <th>Opsi</th>
+            <th>Merek Produk</th>
+            <th>Nama Produk</th>
+            <th>Deskripsi Produk</th>
+            <th>Harga</th>
+            <th>Stok Barang</th>
+            <th>Foto</th>
+        </tr>
+        <?php $i = 1;?>
+        <?php foreach ($barang as $brg) : ?>
+            <tr>
+                <td><?= $i; ?></td>
+                <td>
+                    <a href=""><button>Ubah</button></a>
+                    <a href=""><button>Hapus</button></a>
+                </td>
+                <td><?= $brg["merek"]; ?></td>
+                <td><?= $brg["nama"]; ?></td>
+                <td><?= $brg["deskripsi"]; ?></td>
+                <td><?= $brg["harga"]; ?></td>
+                <td><?= $brg["stok"]; ?></td>
+                <td><img src="../assets/img/<?= $brg['foto']; ?>" alt=""></td>
+            </tr>
+            <?php $i++; ?>
+        <?php endforeach; ?>
+    </table>
+
+</body>
+</html>
---
+++ praktikum/tugas5/latihan5d/php/tambah.php
@@ -0,0 +1,79 @@
+<!-- 
+Nama : Asril Dwi Cahya Permana
+NRP : 203040118
+Shift Praktikum : Jum'at pukul 13:00
+ -->
+
+<?php 
+require 'functions.php';
+
+if (isset($_POST['tambah'])) {
+    if (tambah($_POST) > 0) {
+        echo "<script>
+                    alert('Data Berhasil ditambahkan!');
+                    document.location.href = 'admin.php';
+              </script>";
+    } else {
+        echo    "<script>
+                    alert('Data Gagal ditambahkan!');
+                    document.location.href = 'admin.php';
+                </script>";
+    }
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <title>Latihan 5d</title>
+</head>
+
+<body>
+    <h3>Form Tambah Data Jam Tangan</h3>
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="merek">Merek Produk :</label><br>
+                <input type="text" name="merek" id="merek" required><br><br>
+            </li>
+
+            <li>
+                <label for="nama">Nama Produk :</label><br>
+                <input type="text" name="nama" id="nama" required><br><br>
+            </li>
+
+            <li>
+                <label for="deskripsi">Deskripsi Produk :</label><br>
+                <input type="text" name="deskripsi" id="deskripsi" required><br><br>
+            </li>
+
+            <li>
+                <label for="harga">Harga :</label><br>
+                <input type="text" name="harga" id="harga" required><br><br>
+            </li>
+
+            <li>
+                <label for="stok">Stok Barang :</label><br>
+                <input type="text" name="stok" id="stok" required><br><br>
+            </li>
+
+            <li>
+                <label for="foto">Foto Barang :</label><br>
+                <input type="text" name="foto" id="foto" required><br><br>
+            </li>
+
+            <br>
+            <br><br><button type="submit" name="tambah">Tambah Data!</button>
+            <button type="submit">
+                <a href="index.php" style="text-decoration: none; color: black;">Kembali</a>
+            </button>
+        </ul>
+    </form>
+</body>
+</html>
